8368a669a4a28691229fe4593a359db82c60b958
Charlles Abreu <craabreu@mit.edu>
2025-03-09 17:17:04 -0400

    Added doctest for utils.get_cip_code

--- chempropstereo/featurizers/utils.py
+++ chempropstereo/featurizers/utils.py
@@ -5,7 +5,7 @@ _CIP_CODE_MAP = {False: 0, "R": 1, "S": 2}
 
 def get_cip_code(atom: Chem.Atom) -> int:
     """
-    Get the CIP code of an atom.
+    Get the CIP code of an atom as an integer.
 
     Parameters
     ----------
@@ -15,6 +15,17 @@ def get_cip_code(atom: Chem.Atom) -> int:
     Returns
     -------
     int
-        The CIP code of the atom.
+        The CIP code of the atom: 0 for no CIP code, 1 for R, 2 for S.
+
+    Examples
+    --------
+    >>> from chempropstereo.featurizers.utils import get_cip_code
+    >>> from rdkit import Chem
+    >>> mol1 = Chem.MolFromSmiles("C[C@H](N)O")
+    >>> [get_cip_code(atom) for atom in mol1.GetAtoms()]
+    [0, 1, 0, 0]
+    >>> mol2 = Chem.MolFromSmiles("C[C@@H](N)O")
+    >>> [get_cip_code(atom) for atom in mol2.GetAtoms()]
+    [0, 2, 0, 0]
     """
     return _CIP_CODE_MAP[atom.HasProp("_CIPCode") and atom.GetProp("_CIPCode")]
