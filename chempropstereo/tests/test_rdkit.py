890408003d0b6eee15f9fb8ced2d22cee390618b
Charlles Abreu <craabreu@mit.edu>
2025-03-15 10:43:48 -0400

    Updated rdkit tests

--- chempropstereo/tests/test_rdkit.py
+++ chempropstereo/tests/test_rdkit.py
@@ -1,67 +1,43 @@
+import numpy as np
 from rdkit import Chem
 
 
-def test_stereoperception_setting():
-    """
-    Test that the default stereo perception is set to True and can be set to False
-    """
-    assert Chem.GetUseLegacyStereoPerception() is True
-    Chem.SetUseLegacyStereoPerception(False)
-    assert Chem.GetUseLegacyStereoPerception() is False
-    Chem.SetUseLegacyStereoPerception(True)
+def test_chiral_center_detection():
+    def check_molecule(smiles, descriptions, use_legacy):
+        Chem.SetUseLegacyStereoPerception(use_legacy)
+        mol = Chem.MolFromSmiles(smiles)
+        info_list = Chem.FindPotentialStereo(mol, cleanIt=False, flagPossible=False)
+        assert len(info_list) == len(descriptions)
+        for center, desc in zip(info_list, descriptions):
+            assert center.type == Chem.StereoType.Atom_Tetrahedral
+            assert center.specified == Chem.StereoSpecified.Specified
+            atom = mol.GetAtomWithIdx(center.centeredOn)
+            neighbors = [n.GetIdx() for n in atom.GetNeighbors()]
+            assert atom.GetChiralTag() == getattr(
+                Chem.ChiralType, f"CHI_TETRAHEDRAL_{desc}"
+            )
+            assert center.descriptor == getattr(Chem.StereoDescriptor, f"Tet_{desc}")
+            assert all(i == j for i, j in zip(center.controllingAtoms, neighbors))
+        for atom in mol.GetAtoms():
+            if atom.GetIdx() not in [center.centeredOn for center in info_list]:
+                assert atom.GetChiralTag() == Chem.ChiralType.CHI_UNSPECIFIED
+
+    original_use_legacy = Chem.GetUseLegacyStereoPerception()
+    for use_legacy in [True, False]:
+        check_molecule("C[C@@H](O)N", ["CW"], use_legacy)
+        check_molecule("C[C@H](O)N", ["CCW"], use_legacy)
+        check_molecule("C[C@H](N)O", ["CCW"], use_legacy)
+        check_molecule("C[C@@H](O)[C@@H](C)N", ["CW", "CW"], use_legacy)
+        check_molecule("C[C@@H](O)[C@H](C)N", ["CW", "CCW"], use_legacy)
+    Chem.SetUseLegacyStereoPerception(original_use_legacy)
 
 
-def test_find_potential_stereo():
+def test_canonical_ranking():
     mol = Chem.MolFromSmiles("C[C@@H]([C@@H](C)N)O")
-    info_list = Chem.FindPotentialStereo(mol, cleanIt=False, flagPossible=False)
-    assert len(info_list) == 2
-    c1, c2 = info_list
-
-    assert c1.centeredOn == 1
-    assert list(c1.controllingAtoms) == [0, 2, 5]
-    assert c1.descriptor == Chem.StereoDescriptor.Tet_CW
-    assert c1.specified == Chem.StereoSpecified.Specified
-    assert c1.type == Chem.StereoType.Atom_Tetrahedral
-
-    assert c2.centeredOn == 2
-    assert list(c2.controllingAtoms) == [1, 3, 4]
-    assert c2.descriptor == Chem.StereoDescriptor.Tet_CW
-    assert c2.specified == Chem.StereoSpecified.Specified
-    assert c2.type == Chem.StereoType.Atom_Tetrahedral
-
-    mol = Chem.MolFromSmiles("C[C@H](O)[C@@H](C)N")
-    info_list = Chem.FindPotentialStereo(mol, cleanIt=False, flagPossible=False)
-    assert len(info_list) == 2
-    c1, c2 = info_list
-
-    assert c1.centeredOn == 1
-    assert list(c1.controllingAtoms) == [0, 2, 3]
-    assert c1.descriptor == Chem.StereoDescriptor.Tet_CCW
-    assert c1.specified == Chem.StereoSpecified.Specified
-    assert c1.type == Chem.StereoType.Atom_Tetrahedral
-
-    assert c2.centeredOn == 3
-    assert list(c2.controllingAtoms) == [1, 4, 5]
-    assert c2.descriptor == Chem.StereoDescriptor.Tet_CW
-    assert c2.specified == Chem.StereoSpecified.Specified
-    assert c2.type == Chem.StereoType.Atom_Tetrahedral
-
-    mol = Chem.MolFromSmiles("CC(C(C)N)O")
-    info_list = Chem.FindPotentialStereo(mol, cleanIt=False, flagPossible=False)
-    assert len(info_list) == 0
-
-    info_list = Chem.FindPotentialStereo(mol, cleanIt=False, flagPossible=True)
-    assert len(info_list) == 2
-    c1, c2 = info_list
-
-    assert c1.centeredOn == 1
-    assert list(c1.controllingAtoms) == [0, 2, 5]
-    assert c1.type == Chem.StereoType.Atom_Tetrahedral
-    assert c1.specified == Chem.StereoSpecified.Unspecified
-    assert c1.descriptor == Chem.StereoDescriptor.NoValue
-
-    assert c2.centeredOn == 2
-    assert list(c2.controllingAtoms) == [1, 3, 4]
-    assert c2.type == Chem.StereoType.Atom_Tetrahedral
-    assert c2.specified == Chem.StereoSpecified.Unspecified
-    assert c2.descriptor == Chem.StereoDescriptor.NoValue
+    order = np.argsort(Chem.CanonicalRankAtoms(mol)).tolist()
+    assert order == [0, 3, 4, 5, 1, 2]
+    mol = Chem.AddHs(mol)
+    order = np.argsort(Chem.CanonicalRankAtoms(mol)).tolist()
+    assert order == [16, 14, 15, 6, 7, 8, 11, 12, 13, 9, 10, 5, 4, 0, 3, 1, 2]
+    ranked_elements = "".join([mol.GetAtomWithIdx(i).GetSymbol() for i in order])
+    assert ranked_elements == "HHHHHHHHHHHONCCCC"  # Hydrogens first
