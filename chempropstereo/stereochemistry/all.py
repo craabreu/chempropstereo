679e7ced02a0bd62d5f8848e3f49457d47b3613b
Charlles Abreu <craabreu@mit.edu>
2025-03-24 17:06:40 -0400

    Included tetrahedral stereochemistry in relative neighbor ranking

--- chempropstereo/stereochemistry/all.py
+++ chempropstereo/stereochemistry/all.py
@@ -50,14 +50,40 @@ def tag_stereogroups(mol: Chem.Mol, force: bool = False) -> None:
     tag_cis_trans_stereobonds(mol, force)
 
 
+def is_odd_permutation(i: int, j: int, k: int, m: int | None = None) -> int:
+    r"""Check if a permutation is odd.
+
+    Parameters
+    ----------
+    i
+        The first index.
+    j
+        The second index.
+    k
+        The third index.
+    m
+        The fourth index, optional.
+
+    Returns
+    -------
+    bool
+        Whether the permutation is odd.
+
+    """
+    swaps = int(i > j) + int(i > k) + int(j > k)
+    if m is not None:
+        swaps += int(i > m) + int(j > m) + int(k > m)
+    return bool(swaps % 2)
+
+
 def set_relative_neighbor_ranking(
     mol: Chem.Mol, break_ties: bool = False, force: bool = False
 ) -> None:
     r"""Add neighbor ranking information to the bonds of a molecule.
 
     Neighbors of each atom are sorted in descending order based on their RDKit canonical
-    ranks (see :rdmolfiles:`CanonicalRankAtoms`). This sorting keeps hydrogens at the
-    end of the list.
+    ranks (see :rdmolfiles:`CanonicalRankAtoms`). The descending order aims to keep
+    hydrogens at the end of the list.
 
     For each bond in the molecule, two integer properties are added:
 
@@ -95,10 +121,10 @@ def set_relative_neighbor_ranking(
     >>> from chempropstereo.stereochemistry.utils import describe_atom
     >>> from rdkit import Chem
     >>> import numpy as np
-    >>> mol = Chem.MolFromSmiles("NC(O)=C(O)O")
-    >>> for break_ties in [False, True]:
-    ...     stereochemistry.set_relative_neighbor_ranking(mol, break_ties, force=True)
-    ...     print("\nBreak ties:", ["No", "Yes"][break_ties])
+
+    Define a function for printing bond rankings
+
+    >>> def print_rankings(mol):
     ...     ranks = {describe_atom(atom): [] for atom in mol.GetAtoms()}
     ...     for bond in mol.GetBonds():
     ...         begin = describe_atom(bond.GetBeginAtom())
@@ -106,23 +132,42 @@ def set_relative_neighbor_ranking(
     ...         ranks[begin].append((end, bond.GetIntProp("endRankFromBegin")))
     ...         ranks[end].append((begin, bond.GetIntProp("beginRankFromEnd")))
     ...     for atom in ranks:
-    ...         print(atom, *sorted(ranks[atom], key=lambda x: x[1]))
-    <BLANKLINE>
-    Break ties: No
-    N0 ('C1', 0)
-    C1 ('C3', 0) ('O2', 1) ('N0', 2)
-    O2 ('C1', 0)
-    C3 ('C1', 0) ('O4', 1) ('O5', 1)
-    O4 ('C3', 0)
-    O5 ('C3', 0)
+    ...         if len(ranks[atom]) > 1:
+    ...             print(atom, *sorted(ranks[atom], key=lambda x: x[1]))
+
+    Assign neighbor ranks to a molecule with and without breaking ties
+
+    >>> mol = Chem.MolFromSmiles("NC(O)=C(OC)OC")
+    >>> for break_ties in [True, False]:
+    ...     stereochemistry.set_relative_neighbor_ranking(mol, break_ties, force=True)
+    ...     print("\nBreak ties:", ["No", "Yes"][break_ties])
+    ...     print_rankings(mol)
     <BLANKLINE>
     Break ties: Yes
-    N0 ('C1', 0)
     C1 ('C3', 0) ('O2', 1) ('N0', 2)
-    O2 ('C1', 0)
-    C3 ('C1', 0) ('O5', 1) ('O4', 2)
-    O4 ('C3', 0)
-    O5 ('C3', 0)
+    C3 ('C1', 0) ('O6', 1) ('O4', 2)
+    O4 ('C3', 0) ('C5', 1)
+    O6 ('C3', 0) ('C7', 1)
+    <BLANKLINE>
+    Break ties: No
+    C1 ('C3', 0) ('O2', 1) ('N0', 2)
+    C3 ('C1', 0) ('O4', 1) ('O6', 1)
+    O4 ('C3', 0) ('C5', 1)
+    O6 ('C3', 0) ('C7', 1)
+
+    Assign neighbor ranks to a molecule with a tetrahedral chiral center
+
+    >>> for smiles in ["C[C@](O)(S)N", "C[C@@](O)(S)N"]:
+    ...     print(f"\nMolecule: {smiles}")
+    ...     mol = Chem.MolFromSmiles(smiles)
+    ...     stereochemistry.set_relative_neighbor_ranking(mol)
+    ...     print_rankings(mol)
+    <BLANKLINE>
+    Molecule: C[C@](O)(S)N
+    C1 ('S3', 0) ('N4', 1) ('O2', 2) ('C0', 3)
+    <BLANKLINE>
+    Molecule: C[C@@](O)(S)N
+    C1 ('S3', 0) ('O2', 1) ('N4', 2) ('C0', 3)
 
     """
     if (
@@ -131,7 +176,6 @@ def set_relative_neighbor_ranking(
         and not force
     ):
         return
-    # Negative ranks from Chem.CanonicalRankAtoms to ensure hydrogen atoms are last
     all_priorities = -np.fromiter(
         Chem.CanonicalRankAtoms(
             mol, breakTies=break_ties, includeChirality=False, includeAtomMaps=False
@@ -145,6 +189,14 @@ def set_relative_neighbor_ranking(
         )
         neighbor_priorities = all_priorities[neighbors]
         ranks = np.searchsorted(np.unique(neighbor_priorities), neighbor_priorities)
+
+        # Handle tetrahedral stereocenters
+        chiral_tag = atom.GetChiralTag()
+        is_cw = chiral_tag == Chem.ChiralType.CHI_TETRAHEDRAL_CW
+        is_ccw = chiral_tag == Chem.ChiralType.CHI_TETRAHEDRAL_CCW
+        if (is_cw or is_ccw) and (is_cw == is_odd_permutation(*ranks)):
+            ranks = [3 - rank if 0 < rank < 3 else rank for rank in ranks]
+
         sorted_neighbors.append(dict(zip(neighbors, ranks)))
     for bond in mol.GetBonds():
         begin, end = bond.GetBeginAtomIdx(), bond.GetEndAtomIdx()
